f6f08344fbdfc7f63eccf853886f312b5a2b5685
UserBlackBox <userblackbox@tutanota.com>
2022-01-21 22:20:51 -0500

    create python setup files

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ctf_functions",
+    packages=find_packages(),
+    version="0.1.0",
+    entry_points={
+    },
+    author="Ivy Fan-Chiang",
+    author_email="userblackbox@tutanota.com",
+    description="Python library with functions useful for CTF data analysis/decoding",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UserBlackBox/ctf_functions",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ],
+    python_requires='>=3.6',
+    install_requires=[]
+)
