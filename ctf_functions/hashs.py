5cf1e0d171068653318c60b911ec34f9f8b3f23d
UserBlackBox <userblackbox@tutanota.com>
2022-01-24 11:28:29 -0500

    make imports private in hash module

--- ctf_functions/hashs.py
+++ ctf_functions/hashs.py
@@ -1,130 +1,131 @@
 """Module with functions for hashing bytes and strings"""
 
-import hashlib
-from typing import Dict, List
+import hashlib as __hashlib
+from typing import Dict as __Dict
+from typing import List as __List
 
 
 def str_md5(data: str) -> str:
     """Returns hexadecimal representation of md5 hashed string"""
-    return hashlib.md5(data.encode("utf-8")).hexdigest()
+    return __hashlib.md5(data.encode("utf-8")).hexdigest()
 
 
 def bytes_md5(data: bytes) -> str:
     """Returns hexadecimal representation of md5 hashed bytes"""
-    return hashlib.md5(data).hexdigest()
+    return __hashlib.md5(data).hexdigest()
 
 
 def str_sha1(data: str) -> str:
     """Returns hexadecimal representation of sha1 hashed string"""
-    return hashlib.sha1(data.encode("utf-8")).hexdigest()
+    return __hashlib.sha1(data.encode("utf-8")).hexdigest()
 
 
 def bytes_sha1(data: bytes) -> str:
     """Returns hexadecimal representation of sha1 hashed bytes"""
-    return hashlib.sha1(data).hexdigest()
+    return __hashlib.sha1(data).hexdigest()
 
 
 def str_sha224(data: str) -> str:
     """Returns hexadecimal representation of sha224 hashed string"""
-    return hashlib.sha224(data.encode("utf-8")).hexdigest()
+    return __hashlib.sha224(data.encode("utf-8")).hexdigest()
 
 
 def bytes_sha224(data: bytes) -> str:
     """Returns hexadecimal representation of sha224 hashed bytes"""
-    return hashlib.sha224(data).hexdigest()
+    return __hashlib.sha224(data).hexdigest()
 
 
 def str_sha256(data: str) -> str:
     """Returns hexadecimal representation of sha256 hashed string"""
-    return hashlib.sha256(data.encode("utf-8")).hexdigest()
+    return __hashlib.sha256(data.encode("utf-8")).hexdigest()
 
 
 def bytes_sha256(data: bytes) -> str:
     """Returns hexadecimal representation of sha256 hashed bytes"""
-    return hashlib.sha256(data).hexdigest()
+    return __hashlib.sha256(data).hexdigest()
 
 
 def str_sha384(data: str) -> str:
     """Returns hexadecimal representation of sha384 hashed string"""
-    return hashlib.sha384(data.encode("utf-8")).hexdigest()
+    return __hashlib.sha384(data.encode("utf-8")).hexdigest()
 
 
 def bytes_sha384(data: bytes) -> str:
     """Returns hexadecimal representation of sha384 hashed bytes"""
-    return hashlib.sha384(data).hexdigest()
+    return __hashlib.sha384(data).hexdigest()
 
 
 def str_sha512(data: str) -> str:
     """Returns hexadecimal representation of sha512 hashed string"""
-    return hashlib.sha512(data.encode("utf-8")).hexdigest()
+    return __hashlib.sha512(data.encode("utf-8")).hexdigest()
 
 
 def bytes_sha512(data: bytes) -> str:
     """Returns hexadecimal representation of sha512 hashed bytes"""
-    return hashlib.sha512(data).hexdigest()
+    return __hashlib.sha512(data).hexdigest()
 
 
 def str_sha3_224(data: str) -> str:
     """Returns hexadecimal representation of sha3-224 hashed string"""
-    return hashlib.sha3_224(data.encode("utf-8")).hexdigest()
+    return __hashlib.sha3_224(data.encode("utf-8")).hexdigest()
 
 
 def bytes_sha3_224(data: bytes) -> str:
     """Returns hexadecimal representation of sha3-224 hashed bytes"""
-    return hashlib.sha3_224(data).hexdigest()
+    return __hashlib.sha3_224(data).hexdigest()
 
 
 def str_sha3_256(data: str) -> str:
     """Returns hexadecimal representation of sha3-256 hashed string"""
-    return hashlib.sha3_256(data.encode("utf-8")).hexdigest()
+    return __hashlib.sha3_256(data.encode("utf-8")).hexdigest()
 
 
 def bytes_sha3_256(data: bytes) -> str:
     """Returns hexadecimal representation of sha3-256 hashed bytes"""
-    return hashlib.sha3_256(data).hexdigest()
+    return __hashlib.sha3_256(data).hexdigest()
 
 
 def str_sha3_384(data: str) -> str:
     """Returns hexadecimal representation of sha3-384 hashed string"""
-    return hashlib.sha3_384(data.encode("utf-8")).hexdigest()
+    return __hashlib.sha3_384(data.encode("utf-8")).hexdigest()
 
 
 def bytes_sha3_384(data: bytes) -> str:
     """Returns hexadecimal representation of sha3-384 hashed bytes"""
-    return hashlib.sha3_384(data).hexdigest()
+    return __hashlib.sha3_384(data).hexdigest()
 
 
 def str_sha3_512(data: str) -> str:
     """Returns hexadecimal representation of sha3-512 hashed string"""
-    return hashlib.sha3_512(data.encode("utf-8")).hexdigest()
+    return __hashlib.sha3_512(data.encode("utf-8")).hexdigest()
 
 
 def bytes_sha3_512(data: bytes) -> str:
     """Returns hexadecimal representation of sha3-512 hashed bytes"""
-    return hashlib.sha3_512(data).hexdigest()
+    return __hashlib.sha3_512(data).hexdigest()
 
 
 def str_blake2b(data: str) -> str:
     """Returns hexadecimal representation of blake2b hashed string"""
-    return hashlib.blake2b(data.encode("utf-8")).hexdigest()
+    return __hashlib.blake2b(data.encode("utf-8")).hexdigest()
 
 
 def bytes_blake2b(data: bytes) -> str:
     """Returns hexadecimal representation of blake2b hashed bytes"""
-    return hashlib.blake2b(data).hexdigest()
+    return __hashlib.blake2b(data).hexdigest()
 
 
 def str_blake2s(data: str) -> str:
     """Returns hexadecimal representation of blake2s hashed string"""
-    return hashlib.blake2s(data.encode("utf-8")).hexdigest()
+    return __hashlib.blake2s(data.encode("utf-8")).hexdigest()
 
 
 def bytes_blake2s(data: bytes) -> str:
     """Returns hexadecimal representation of blake2s hashed bytes"""
-    return hashlib.blake2s(data).hexdigest()
+    return __hashlib.blake2s(data).hexdigest()
 
 
-def str_all_hashes(data: str) -> Dict[str, str]:
+def str_all_hashes(data: str) -> __Dict[str, str]:
     """Returns a dictionary of hexadecimal representations of all available
     hash algorithms for a given string"""
     return {
@@ -143,7 +144,7 @@ def str_all_hashes(data: str) -> Dict[str, str]:
             }
 
 
-def bytes_all_hashes(data: bytes) -> Dict[str, str]:
+def bytes_all_hashes(data: bytes) -> __Dict[str, str]:
     """Returns a dictionary of hexadecimal representations of all available
     hash algorithms for given bytes"""
     return {
@@ -162,8 +163,9 @@ def bytes_all_hashes(data: bytes) -> Dict[str, str]:
             }
 
 
-def analyze_hash(hashhex: str) -> List[str]:
-    """Determines possible algorithms that generated a hash based on its length"""
+def analyze_hash(hashhex: str) -> __List[str]:
+    """Determines possible algorithms that generated
+    a hash based on its length"""
     bits = len(hashhex) // 2 * 8
     algos = {
             32: [
