08134f71e88eda272807f84376faba40171f6768
UserBlackBox <userblackbox@tutanota.com>
2022-01-24 22:52:25 -0500

    hex and binary string encodings

--- ctf_functions/encodings.py
+++ ctf_functions/encodings.py
@@ -2,9 +2,10 @@
 
 import base64 as __b64
 import urllib.parse as __ulp
+from typing import Union as __Union
 
 
-def base64_encode(data: bytes) -> str:
+def base64_encode(data: __Union[str, bytes]) -> str:
     """Encode given data using base64"""
     if isinstance(data, str):
         data = data.encode("utf-8")
@@ -16,7 +17,7 @@ def base64_decode(data: str) -> bytes:
     return __b64.b64decode(data)
 
 
-def base64url_encode(data: bytes) -> str:
+def base64url_encode(data: __Union[str, bytes]) -> str:
     """Encode given data using base64 with url safe alphabet"""
     if isinstance(data, str):
         data = data.encode("utf-8")
@@ -28,7 +29,7 @@ def base64url_decode(data: str) -> bytes:
     return __b64.urlsafe_b64decode(data)
 
 
-def base32_encode(data: bytes) -> str:
+def base32_encode(data: __Union[str, bytes]) -> str:
     """Encode given data using base32"""
     if isinstance(data, str):
         data = data.encode("utf-8")
@@ -40,7 +41,7 @@ def base32_decode(data: str) -> bytes:
     return __b64.b32decode(data)
 
 
-def base16_encode(data: bytes) -> str:
+def base16_encode(data: __Union[str, bytes]) -> str:
     """Encode given data using base16"""
     if isinstance(data, str):
         data = data.encode("utf-8")
@@ -62,3 +63,37 @@ def url_encode(string: str, escape_all: bool = False) -> str:
 def url_decode(string: str) -> str:
     """Decodes a url escaped string"""
     return __ulp.unquote(string)
+
+
+def to_hex(data: __Union[str, bytes], delimiter: str = " ") -> str:
+    """Converts input data to hexadecimal bytes"""
+    hex_encoded = []
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+    for char in data:
+        hex_encoded.append(hex(char)[2:])
+    return delimiter.join(hex_encoded)
+
+
+def from_hex(hex_data: str, delimiter: str = " ") -> bytes:
+    """Converts hexadecimal byte string into bytes object"""
+    data = hex_data.split(delimiter)
+    data = [int(byte, 16) for byte in data]
+    return bytes(data)
+
+
+def to_binary(data: __Union[str, bytes], delimiter: str = " ") -> str:
+    """Converts input data to a binary string"""
+    bin_encoded = []
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+    for char in data:
+        bin_encoded.append(bin(char)[2:])
+    return delimiter.join(bin_encoded)
+
+
+def from_binary(bin_data: str, delimiter: str = " ") -> bytes:
+    """Converts binary string into bytes object"""
+    data = bin_data.split(delimiter)
+    data = [int(byte, 2) for byte in data]
+    return bytes(data)
