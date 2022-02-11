56b1179b3e31183d057954bd8107907e7abe171e
UserBlackBox <userblackbox@tutanota.com>
2022-02-11 11:35:53 -0500

    create octal encoding function and make fixes for empty delimeters and enssuring encoding has a proper amount of leading zeros

--- ctf_functions/encodings.py
+++ ctf_functions/encodings.py
@@ -72,12 +72,17 @@ def to_hex(data: __Union[str, bytes], delimiter: str = " ") -> str:
         data = data.encode('utf-8')
     for char in data:
         hex_encoded.append(hex(char)[2:])
+        if len(hex_encoded[-1]) == 1:
+            hex_encoded[-1] = "0" + hex_encoded[-1]
     return delimiter.join(hex_encoded)
 
 
 def from_hex(hex_data: str, delimiter: str = " ") -> bytes:
     """Converts hexadecimal byte string into bytes object"""
-    data = hex_data.split(delimiter)
+    if delimiter == "":
+        data = [hex_data[i:i+2] for i in range(0, len(hex_data), 2)]
+    else:
+        data = hex_data.split(delimiter)
     data = [int(byte, 16) for byte in data]
     return bytes(data)
 
@@ -89,23 +94,54 @@ def to_binary(data: __Union[str, bytes], delimiter: str = " ") -> str:
         data = data.encode('utf-8')
     for char in data:
         bin_encoded.append(bin(char)[2:])
+        bin_encoded[-1] = "0" * (8 - len(bin_encoded[-1])) + bin_encoded[-1]
     return delimiter.join(bin_encoded)
 
 
 def from_binary(bin_data: str, delimiter: str = " ") -> bytes:
     """Converts binary string into bytes object"""
-    data = bin_data.split(delimiter)
+    if delimiter == "":
+        data = [bin_data[i:i+8] for i in range(0, len(bin_data), 8)]
+    else:
+        data = bin_data.split(delimiter)
     data = [int(byte, 2) for byte in data]
     return bytes(data)
+
+
+def to_octal(data: __Union[str, bytes], delimiter: str = " ") -> str:
+    """Converts input data to octal string"""
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+    oct_encoded = []
+    for char in data:
+        oct_encoded.append(oct(char)[2:])
+        oct_encoded[-1] = "0" * (3 - len(oct_encoded[-1])) + oct_encoded[-1]
+    return delimiter.join(oct_encoded)
+
+
+def from_octal(oct_data: str, delimiter: str = " ") -> bytes:
+    """Converts octal string into bytes object"""
+    if delimiter == "":
+        data = [oct_data[i:i+3] for i in range(0, len(oct_data), 3)]
+    else:
+        data = oct_data.split(delimiter)
+    return bytes([int(byte, 8) for byte in data])
 
 
 def to_decimal(data: __Union[str, bytes], delimiter: str = " ") -> str:
     """Converts input data to decimal bytes"""
     if isinstance(data, str):
         data = data.encode('utf-8')
-    return delimiter.join(map(str, list(data)))
+    dec_encode = list(map(str, list(data)))
+    for i in range(len(dec_encode)):
+        dec_encode[i] = "0" * (3 - len(dec_encode[i])) + dec_encode[i]
+    return delimiter.join(dec_encode)
 
 
 def from_decimal(dec_data: str, delimiter: str = " ") -> bytes:
     """Converts decimal string into bytes object"""
-    return bytes(map(int, dec_data.split(delimiter)))
+    if delimiter == "":
+        data = [dec_data[i:i+3] for i in range(0, len(dec_data), 3)]
+    else:
+        data = dec_data.split(delimiter)
+    return bytes(map(int, data))
